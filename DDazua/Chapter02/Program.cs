b2e85fb1095d133577ac9db0b8044cdaa1e8c560
Byoungjin <qudwls7221@gmail.com>
2022-12-20 00:16:41 +0900

    firstTimeC#
    setup and setting

---
+++ DDazua/Chapter02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO : 여기 코딩하자.
+        }
+    }
+}
