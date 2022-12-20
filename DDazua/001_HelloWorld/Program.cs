e6f2f5b1f8dac106126a50b79635d917d7484408
Byoungjin <qudwls7221@gmail.com>
2022-12-20 22:27:40 +0900

    Hello,Variable
    visualstudio기본출력방법과 int정수형

---
+++ DDazua/001_HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using MyConsole = System.Console;
+
+namespace _001_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            
+        }
+    }
+}
---
+++ DDazua/002_Variable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수 선언의 기초방법.
+            {
+                //int number = 10;
+                int number;
+                number = 10;
+                Console.WriteLine(number);
+            }
+            //number = 1000;
+            //여기서는 {} 의 외부이기때문에 number 변수는 사용할수없다.
+            //변수 선언의 다양한 방법. 자바의 변수선언방식과 동일하다.
+            {
+                int x;
+                int a, b, c;
+
+                int xx = 100;
+                int aa = 0, bb = 1, cc = 2;
+            }
+        }
+    }
+}
